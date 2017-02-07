6bd26b32bf8d71730bef8ce5d351a8c45597fffb
sandyrocks <sandy.singh21031988@gmail.com>
2017-02-07 19:11:48 +0530

    Added base yml configuration and added allowed attr configurations.

--- lib/generators/model_form_generator.rb
+++ lib/generators/model_form_generator.rb
@@ -2,7 +2,7 @@ class ModelFormGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
   def create_initializer_file
-    copy_file "model_form_config.yml", "#{Rails.root}/config/model_form_config.yml"
+    copy_file "model_form.yml", "#{Rails.root}/config/model_form.yml"
   end
 
 end
--- lib/model_form.rb
+++ lib/model_form.rb
@@ -1,6 +1,6 @@
 module ModelForm
   # takes ActiveRecord object
-  CONFIG_FILE = "/config/model_form_config.yml"
+  CONFIG_FILE = "/config/model_form.yml"
 
   def model_form resource
     simple_form_for resource do |f|
@@ -11,7 +11,7 @@ module ModelForm
   end
 
   def visible_fields resource
-    resource.attribute_names - ignored_attributes(resource)
+    (resource.attribute_names - ignored_attributes(resource)) | allowed_attributes(resource)
   end
 
   def cancel_link resource
@@ -19,20 +19,33 @@ module ModelForm
   end
 
   def ignored_attributes resource
-    config = load_config_file
-    global_config(resource, config) + model_config(resource, config)
+    config = load_config
+    (global_config(resource, config) + model_config(resource, config)).uniq
   end
 
   def global_config resource, config
-    config.fetch("global_ignored_attributes", [])
+    # config.fetch("global_ignored_attributes", [])
+    config.fetch("global", {}).fetch("ignored_attributes", [])
   end
 
   def model_config resource, config
-    config[resource.class.to_s]["ignored_attributes"] || []
+    config.fetch(resource.class.to_s.downcase, {}).fetch("ignored_attributes", [])
   end
 
-  def load_config_file
-    YAML.load_file("#{Rails.root}#{CONFIG_FILE}")
+  def allowed_attributes(resource)
+    config = load_config
+    config.fetch(resource.class.to_s.downcase, {}).fetch("allowed_attributes", [])
+  end
+
+  def load_config
+    file = "#{Rails.root}#{CONFIG_FILE}"
+    base_file = "../generators/templates/model_form.yml"
+    if File.exists?(file)
+      YAML.load_file(file)
+    else
+      YAML.load_file File.expand_path(base_file, __FILE__)
+    end
+
   end
 
   ActionView::Base.send :include, ModelForm
